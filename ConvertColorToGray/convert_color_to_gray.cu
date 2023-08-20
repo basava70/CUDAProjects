8041a269d35c5dbae27cdb7d5893524dae698736
SeshHomeUbuntuPC <seshu.basava70@gmail.com>
2023-08-21 01:19:42 +0200

    Update vector addition and added converting color to gray image

---
+++ ConvertColorToGray/convert_color_to_gray.cu
@@ -0,0 +1,179 @@
+/**
+ * Converting color image to gray
+ * using cuda. Created on August 20
+ *
+ **/
+
+#include <cstddef>
+#include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <stdio.h>
+#include <string>
+
+unsigned char *image_rgb_h;
+#define CHANNELS 3 // number of channels 3 for r g b
+
+// error handling
+inline void cudaErrorCheck(cudaError_t error, bool abort = true) {
+  if (error != cudaSuccess) {
+    printf("%s in %s at line %d\n", cudaGetErrorString(error), __FILE__,
+           __LINE__);
+    if (abort)
+      exit(EXIT_FAILURE);
+  }
+}
+
+void printKernelData(dim3 blockDim, dim3 gridDim) {
+  std::cout << "blockDim(x, y, z) : (" << blockDim.x << ", " << blockDim.y
+            << ", " << blockDim.z << ") " << std::endl;
+  std::cout << "gridDim(x, y, z) : (" << gridDim.x << ", " << gridDim.y << ", "
+            << gridDim.z << ") " << std::endl;
+}
+
+int loadImage(std::string input_file, size_t *rows, size_t *cols) {
+  cv::Mat image;
+  image = cv::imread(input_file.c_str(), cv::IMREAD_COLOR);
+  if (image.empty() || !image.data) {
+    std::cerr << "Unable to load image " << input_file << std::endl;
+    exit(1);
+  }
+  *rows = image.rows;
+  *cols = image.cols;
+
+  // allocate memory for host rgb image variable
+  image_rgb_h =
+      (unsigned char *)malloc(*rows * *cols * sizeof(unsigned char) * 3);
+  // create a tmp variable to copy data from cv::Mat
+  unsigned char *rgb_image = (unsigned char *)image.data;
+
+  // populate host's rgb data array
+  size_t iter = 0;
+  for (iter = 0; iter < *rows * *cols * 3; iter++) {
+    image_rgb_h[iter] = rgb_image[iter];
+  }
+
+  size_t num_of_pixels = image.rows * image.cols;
+
+  return num_of_pixels;
+}
+
+__global__ void convertRGBToGrayGPUKernel(unsigned char *rgb,
+                                          unsigned char *gray, size_t rows,
+                                          size_t cols) {
+
+  size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+  size_t idy = blockIdx.y * blockDim.y + threadIdx.y;
+
+  if (idx <= rows && idy <= cols) {
+    size_t grayoffset = idy * cols + idx;
+    size_t rgboffset = grayoffset * CHANNELS;
+
+    unsigned char r = rgb[rgboffset + 0];
+    unsigned char g = rgb[rgboffset + 1];
+    unsigned char b = rgb[rgboffset + 2];
+
+    gray[grayoffset] = r * 0.299f + g * 0.587f + b * 0.114f;
+  }
+}
+
+void convertRGBToGrayGPU(unsigned char *image_rgb_h,
+                         unsigned char *image_gray_h, const size_t total_pixels,
+                         const size_t rows, const size_t cols,
+                         bool verbose = false) {
+
+  unsigned char *image_gray_d;
+  unsigned char *image_rgb_d;
+
+  // allocating the memory for device variables
+  cudaErrorCheck(cudaMalloc((void **)&image_rgb_d,
+                            total_pixels * sizeof(unsigned char) * CHANNELS));
+  cudaErrorCheck(
+      cudaMalloc((void **)&image_gray_d, total_pixels * sizeof(unsigned char)));
+  cudaErrorCheck(
+      cudaMemset(image_gray_d, 0, total_pixels * sizeof(unsigned char)));
+
+  // copy data from host to device
+  cudaErrorCheck(cudaMemcpy(image_rgb_d, image_rgb_h,
+                            total_pixels * sizeof(unsigned char) * CHANNELS,
+                            cudaMemcpyHostToDevice));
+
+  size_t threads_per_block = 16;
+  dim3 blockDim(16, 16);
+  dim3 gridDim(ceil(float(rows) / threads_per_block),
+               ceil(float(cols) / threads_per_block));
+
+  if (verbose == true)
+    printKernelData(blockDim, gridDim);
+
+  convertRGBToGrayGPUKernel<<<gridDim, blockDim>>>(image_rgb_d, image_gray_d,
+                                                   rows, cols);
+
+  cudaErrorCheck(cudaMemcpy(image_gray_h, image_gray_d,
+                            total_pixels * sizeof(unsigned char),
+                            cudaMemcpyDeviceToHost));
+}
+
+void outputImage(unsigned char *gray, const std::string output_file,
+                 const size_t rows, const size_t cols) {
+
+  cv::Mat output(rows, cols, CV_8UC1, (void *)gray);
+  cv::imwrite(output_file.c_str(), output);
+  cv::imshow("GrayImage", output);
+  cv::waitKey(0);
+}
+
+// Global_variables
+
+int main(int argc, char **argv) {
+  // declaring input and output file variables
+  std::string input_file;
+  std::string output_file;
+
+  switch (argc) {
+  case 2:
+    input_file = std::string(argv[1]);
+    output_file = "output.jpg";
+    break;
+
+  case 3:
+    input_file = std::string(argv[1]);
+    output_file = std::string(argv[2]);
+    break;
+
+  default:
+    std::cerr << "Usage: <executable> input_file output_file" << std::endl;
+    exit(1);
+  }
+
+  // dimension variables in the image
+  size_t rows;
+  size_t cols;
+
+  // lets read the input file
+  size_t total_pixels = loadImage(input_file, &rows, &cols);
+
+  std::cout << "--------------------------" << std::endl;
+  std::cout << " Dimensions of the input " << std::endl;
+  std::cout << " rows : " << rows << std::endl;
+  std::cout << " cols : " << cols << std::endl;
+  std::cout << " total_pixels : " << total_pixels << std::endl;
+  std::cout << "--------------------------" << std::endl;
+
+  cv::Mat output(rows, cols, CV_8UC3, (void *)image_rgb_h);
+  cv::imwrite(output_file.c_str(), output);
+  cv::imshow("rgb_h", output);
+
+  // create host image variable
+  unsigned char *image_gray_h;
+  image_gray_h =
+      (unsigned char *)malloc(total_pixels * sizeof(unsigned char *));
+
+  convertRGBToGrayGPU(image_rgb_h, image_gray_h, total_pixels, rows, cols,
+                      true);
+
+  outputImage(image_gray_h, output_file, rows, cols);
+
+  return 0;
+}
--- VectorAddition/vector_addition.cu
+++ VectorAddition/vector_addition.cu
@@ -5,6 +5,9 @@
  **/
 
 #include <chrono>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
 #include <stdio.h>
 #include <string>
@@ -19,6 +22,7 @@ inline void cudaErrorCheck(cudaError_t error, bool abort = true) {
   }
 }
 
+// gpu kernel
 template <typename T> __global__ void vecAddKernel(T *a, T *b, T *c, size_t n) {
   size_t i = blockIdx.x * blockDim.x + threadIdx.x;
   if (i < n) {
@@ -26,7 +30,13 @@ template <typename T> __global__ void vecAddKernel(T *a, T *b, T *c, size_t n) {
   }
 }
 
-template <typename T> void vecAdd(const T *a, const T *b, T *c, size_t n) {
+/**
+ * This function does all the necessary things for the gpu vector addition.
+ * Creates the three device vectors a_d, b_d and c_d, allocates memory
+ * and sends them to the gpu kernel with necesary threads and blocks.
+ * Frees the memory from gpu as well.
+ **/
+template <typename T> size_t vecAdd(const T *a, const T *b, T *c, size_t n) {
 
   // creating device variables
   T *a_d, *b_d, *c_d;
@@ -42,15 +52,25 @@ template <typename T> void vecAdd(const T *a, const T *b, T *c, size_t n) {
 
   // variables for defining kernels
   size_t threads_per_block = 256;
+  size_t blockDim = ceil(float(n) / threads_per_block);
 
   // assigning kernel
-  vecAddKernel<<<ceil(float(n) / threads_per_block), threads_per_block>>>(
-      a_d, b_d, c_d, n);
+  vecAddKernel<<<blockDim, threads_per_block>>>(a_d, b_d, c_d, n);
 
   // copy the result device variable c_d back to the host variable c_h
   cudaErrorCheck(cudaMemcpy(c, c_d, n * sizeof(T), cudaMemcpyDeviceToHost));
+
+  // free cuda memory
+  cudaFree(a_d);
+  cudaFree(b_d);
+  cudaFree(c_d);
+
+  return threads_per_block;
 }
 
+/**
+ * The cpu kernel for addition to compare computation time
+ **/
 template <typename T> void vecAddCPU(const T *a, const T *b, T *c, size_t n) {
   for (int i = 0; i < n; i++) {
     c[i] = a[i] + b[i];
@@ -69,10 +89,32 @@ int main() {
   b_h = (float *)malloc(size);
   c_h = (float *)malloc(size);
 
+  for (int i = 0; i < n; i++) {
+    a_h[i] = i;
+    b_h[i] = 2 * i;
+  }
+
   // measuring time for the gpu version
   auto begin_gpu = std::chrono::high_resolution_clock::now();
-  vecAdd(a_h, b_h, c_h, n);
+  size_t threads_per_block = vecAdd(a_h, b_h, c_h, n);
   auto end_gpu = std::chrono::high_resolution_clock::now();
+
+  // checking for correctness
+  double tolerance = 1e-14;
+  for (size_t iter = 0; iter < n; iter++) {
+    if (fabs(c_h[iter] - 3.0 * iter) > tolerance) {
+      std::cout << "Error c_h[" << iter << "] = " << c_h[iter] << "instead of "
+                << 3 * iter << std::endl;
+      exit(1);
+    }
+  }
+
+  std::cout << " ------------------------ " << std::endl;
+  std::cout << " ------- Success -------- " << std::endl;
+  std::cout << " Threads used : " << threads_per_block << std::endl;
+  std::cout << " Blocks used : " << ceil(float(n) / threads_per_block)
+            << std::endl;
+  std::cout << " ------------------------ " << std::endl;
 
   std::cout << "Time elapsed for gpu is: "
             << std::chrono::duration_cast<std::chrono::nanoseconds>(end_gpu -
@@ -90,6 +132,11 @@ int main() {
                                                                     begin_cpu)
                    .count()
             << std::endl;
+
+  // free cpu memory
+  free(a_h);
+  free(b_h);
+  free(c_h);
 
   return 0;
 }
