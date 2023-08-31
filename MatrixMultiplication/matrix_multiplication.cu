4b1c3c04385bde2054fa4f7081998aeee1baafb4
SeshHomeUbuntuPC <seshu.basava70@gmail.com>
2023-08-31 14:21:22 +0200

    matrix multiplication for rectangular matrices

--- MatrixMultiplication/matrix_multiplication.cu
+++ MatrixMultiplication/matrix_multiplication.cu
@@ -31,13 +31,8 @@
  **/
 
 #include <chrono>
-#include <cstddef>
+#include <cstdlib>
 #include <iostream>
-#include <stdio.h>
-#include <time.h>
-
-size_t row_A = 512, row_B = 1024, col_A = 1024, col_B = 512;
-// size_t row_A = 3, row_B = 3, col_A = 3, col_B = 3;
 
 // Error handling
 inline void cudaErrorCheck(cudaError_t error, bool abort = true) {
@@ -57,58 +52,76 @@ void printKernelData(dim3 blockDim, dim3 gridDim) {
 }
 
 // Initialize the matrices A and B randomly between -50 to 50
-void initialize_data(int *A, int *B, int *C_cpu, int *C_gpu) {
+void initialize_data(int *A, int *B, int *C_cpu, int *C_gpu, const size_t row_A,
+                     const size_t row_B, const size_t col_B) {
+
   // Initialize to a random seed
-  srand(22);
+  // srand((unsigned)time(NULL));
+  srand(1);
+  std::cout << "Started initialization " << std::endl;
   for (size_t i = 0; i < row_A; i++) {
-    for (size_t j = 0; j < col_A; j++) {
-      A[j * col_A + i] = rand() % 10;
-      // A[j * col_A + i] = rand() % 101 - 50;
+    for (size_t j = 0; j < row_B; j++) {
+      A[i * row_B + j] = (rand() % 10);
     }
   }
 
   for (size_t i = 0; i < row_B; i++) {
     for (size_t j = 0; j < col_B; j++) {
-      B[j * col_B + i] = rand() % 10;
-      // B[j * col_B + i] = rand() % 101 - 50;
+      B[i * col_B + j] = (rand() % 10);
     }
   }
 
   for (size_t i = 0; i < row_A; i++) {
     for (size_t j = 0; j < col_B; j++) {
-      C_cpu[j * col_B + i] = 0;
-      C_gpu[j * col_B + i] = 0;
+      C_cpu[i * col_B + j] = 0;
+      C_gpu[i * col_B + j] = 0;
     }
   }
 }
 
-__global__ void gpuKernel(const int *a, const int *b, int *c, const int row_b,
-                          const size_t rows, const size_t cols) {
+void cpuMatrixMultiplication(const int *A, const int *B, int *C,
+                             const size_t row_A, const size_t row_B,
+                             const size_t col_B) {
+  for (size_t i = 0; i < row_A; i++) {
+    for (size_t j = 0; j < col_B; j++) {
+      int value = 0;
+      for (size_t k = 0; k < row_B; k++) {
+        value += A[i * row_B + k] * B[k * col_B + j];
+      }
+      C[i * col_B + j] = value;
+    }
+  }
+}
 
+__global__ void gpuKernel(const int *a, const int *b, int *c,
+                          const size_t row_A, const size_t row_B,
+                          const size_t col_B) {
   int idx = threadIdx.x + blockIdx.x * blockDim.x;
   int idy = threadIdx.y + blockIdx.y * blockDim.y;
 
-  if (idx < rows && idy < cols) {
+  if (idx < row_A && idy < col_B) {
     int value = 0;
-    for (size_t i = 0; i < row_b; i++) {
-      value += a[i * row_b + idx] * b[idy * cols + i];
+    for (size_t k = 0; k < row_B; k++) {
+      value += a[idx * row_B + k] * b[k * col_B + idy];
     }
-    c[idy * cols + idx] = value;
+    c[idx * col_B + idy] = value;
   }
 }
 
-void gpuMatrixMultiplication(const int *A, const int *B, int *C) {
+void gpuMatrixMultiplication(const int *A, const int *B, int *C,
+                             const size_t row_A, const size_t row_B,
+                             const size_t col_B) {
 
   // Creating device variables for all the three matrices
   int *a_d, *b_d, *c_d;
 
   // Allocating memory for device variables
-  cudaErrorCheck(cudaMalloc((void **)&a_d, row_A * col_A * sizeof(int)));
+  cudaErrorCheck(cudaMalloc((void **)&a_d, row_A * row_B * sizeof(int)));
   cudaErrorCheck(cudaMalloc((void **)&b_d, row_B * col_B * sizeof(int)));
   cudaErrorCheck(cudaMalloc((void **)&c_d, row_A * col_B * sizeof(int)));
 
   cudaErrorCheck(
-      cudaMemcpy(a_d, A, row_A * col_A * sizeof(int), cudaMemcpyHostToDevice));
+      cudaMemcpy(a_d, A, row_A * row_B * sizeof(int), cudaMemcpyHostToDevice));
   cudaErrorCheck(
       cudaMemcpy(b_d, B, row_B * col_B * sizeof(int), cudaMemcpyHostToDevice));
 
@@ -119,9 +132,7 @@ void gpuMatrixMultiplication(const int *A, const int *B, int *C) {
 
   printKernelData(block_dimension, grid_dimension);
 
-  const size_t row_b = row_B;
-
-  gpuKernel<<<grid_dimension, block_dimension>>>(a_d, b_d, c_d, row_b, row_A,
+  gpuKernel<<<grid_dimension, block_dimension>>>(a_d, b_d, c_d, row_A, row_B,
                                                  col_B);
 
   cudaErrorCheck(
@@ -132,53 +143,59 @@ void gpuMatrixMultiplication(const int *A, const int *B, int *C) {
   cudaFree(c_d);
 }
 
-void cpuMatrixMultiplication(const int *A, const int *B, int *C) {
-  int value = 0;
-  for (size_t i = 0; i < row_A; i++) {
-    for (size_t j = 0; j < col_B; j++) {
-      value = 0;
-      for (size_t k = 0; k < row_B; k++) {
-        value += A[k * col_A + i] * B[j * col_B + k];
-      }
-      C[j * col_B + i] = value;
-    }
-  }
-}
-
-void error_check(const int *C_h, const int *C_d) {
+void error_check(const int *C_h, const int *C_d, const size_t row_A,
+                 const size_t col_B) {
   const double tolerance = 1e-4;
   for (size_t i = 0; i < row_A; i++) {
     for (size_t j = 0; j < col_B; j++) {
-      if (C_h[j * col_B + i] - C_d[j * col_B + i] > tolerance) {
+      if (C_h[i * col_B + j] - C_d[i * col_B + j] > tolerance) {
         std::cout << "Error C_cpu(" << i << ", " << j
-                  << ") = " << C_h[j * col_B + i] << " and C_gpu(" << i << ", "
-                  << j << ") = " << C_d[j * col_B + i] << std::endl;
+                  << ") = " << C_h[i * col_B + j] << " and C_gpu(" << i << ", "
+                  << j << ") = " << C_d[i * col_B + j] << std::endl;
         exit(1);
       }
     }
   }
 }
 
-// void print_matrix(const int *A) {
-//   for (size_t i = 0; i < row_A; i++) {
-//     for (size_t j = 0; j < col_B; j++) {
-//       std::cout << " " << A[j * row_A + i];
-//     }
-//     std::cout << std::endl;
-//   }
-// }
+void print_matrix(const int *A, const size_t row_A, const size_t col_A) {
+  for (size_t i = 0; i < row_A; i++) {
+    std::cout << "|";
+    for (size_t j = 0; j < col_A; j++) {
+      std::cout << " " << A[i * col_A + j];
+    }
+    std::cout << " |";
+    std::cout << std::endl;
+  }
+}
 
 int main() {
 
+  const size_t row_A = 512, row_B = 1024, col_A = 1024, col_B = 512;
   int A[row_A * col_A], B[row_B * col_B], C_cpu[row_A * col_B];
+
+  if (row_B != col_A) {
+    std::cout << " row_b != col_A " << std::endl;
+    exit(1);
+  }
 
   int *C_gpu;
   C_gpu = (int *)malloc(row_A * col_B * sizeof(int));
 
-  initialize_data(&A[0], &B[0], &C_cpu[0], C_gpu);
+  std::cout << " /***********************************************/ "
+            << std::endl;
+  std::cout << "  *************Matrix multiplication*************  "
+            << std::endl;
+  std::cout << "  /***********************************************/ "
+            << std::endl;
+  std::cout << " row_A : " << row_A << " col_A : " << col_A << std::endl;
+  std::cout << " row_B : " << row_B << " col_B : " << col_B << std::endl;
+  std::cout << " row_C : " << row_A << " col_C : " << col_B << std::endl;
+
+  initialize_data(&A[0], &B[0], &C_cpu[0], C_gpu, row_A, row_B, col_B);
 
   auto begin_gpu = std::chrono::high_resolution_clock::now();
-  gpuMatrixMultiplication(&A[0], &B[0], C_gpu);
+  gpuMatrixMultiplication(&A[0], &B[0], C_gpu, row_A, row_B, col_B);
   auto end_gpu = std::chrono::high_resolution_clock::now();
 
   std::cout << "Time elapsed for gpu is: "
@@ -188,7 +205,7 @@ int main() {
             << " micro seconds " << std::endl;
 
   auto begin_cpu = std::chrono::high_resolution_clock::now();
-  cpuMatrixMultiplication(&A[0], &B[0], &C_cpu[0]);
+  cpuMatrixMultiplication(&A[0], &B[0], &C_cpu[0], row_A, row_B, col_B);
   auto end_cpu = std::chrono::high_resolution_clock::now();
 
   std::cout << "Time elapsed for cpu is: "
@@ -198,15 +215,15 @@ int main() {
             << " micro seconds " << std::endl;
 
   /* std::cout << " A =" << std::endl;
-  print_matrix(&A[0]);
+  print_matrix(&A[0], row_A, col_A);
   std::cout << " B =" << std::endl;
-  print_matrix(&B[0]);
+  print_matrix(&B[0], row_B, col_B);
   std::cout << " C_cpu =" << std::endl;
-  print_matrix(&C_cpu[0]);
+  print_matrix(&C_cpu[0], row_A, col_B);
   std::cout << " C_gpu =" << std::endl;
-  print_matrix(C_gpu); */
+  print_matrix(C_gpu, row_A, col_B); */
 
-  error_check(&C_cpu[0], &C_gpu[0]);
+  error_check(&C_cpu[0], &C_gpu[0], row_A, col_B);
 
   std::cout << " ------------------------ " << std::endl;
   std::cout << " ------- Success -------- " << std::endl;
